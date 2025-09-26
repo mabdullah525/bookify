77f443c19b6e14a9f0afa8a216a47c3905348e98
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-09-26 05:07:44 -0700

    add browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { FirebaseProvider } from './Context/firebase.jsx' // Import the provider
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <FirebaseProvider/> { /* Wrap App with the provider */ }
+    <BrowserRouter>
+      <App />
+      <FirebaseProvider /> { /* Wrap App with the provider */}
+    </BrowserRouter>
   </StrictMode>,
 )
