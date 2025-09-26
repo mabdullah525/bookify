7ee9baa19840ee950c06373158c201acbf1a8221
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-09-26 02:52:36 -0700

    Firebase configuration

--- src/Context/Firebase.jsx
+++ src/Context/Firebase.jsx
@@ -2,6 +2,16 @@ import { createContext, useContext } from "react";
 export const FirebaseContext = createContext(null);
 // Complete our context 
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAoOnBEUTCvS5NBM8-PMZuG6UO9F3VqSQA",
+    authDomain: "bookify-66100.firebaseapp.com",
+    projectId: "bookify-66100",
+    storageBucket: "bookify-66100.firebasestorage.app",
+    messagingSenderId: "785991373853",
+    appId: "1:785991373853:web:420c2f60557d957c11e2f4"
+};
+
 export const useFirebase = useContext(FirebaseContext);
 // This is a custom hook to use the context
 
