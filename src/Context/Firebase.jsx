6c7bf39cb120c8fa0912c9e6692a929b990daa7e
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-09-29 02:42:24 -0700

    If user login or not Function

--- src/Context/Firebase.jsx
+++ src/Context/Firebase.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 import { initializeApp } from "firebase/app"; // Import the functions you need from the SDKs you need
 export const FirebaseContext = createContext(null);
 // Complete our context 
@@ -7,7 +7,8 @@ import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     GoogleAuthProvider,
-    signInWithPopup
+    signInWithPopup,
+    onAuthStateChanged, // to detect user login or not ?
 } from "firebase/auth"; // Import Firebase Authentication
 
 // Your web app's Firebase configuration
@@ -22,13 +23,20 @@ const firebaseConfig = {
 
 export const useFirebase = () => useContext(FirebaseContext);
 // This is a custom hook to use the context
-
 const firebaseApp = initializeApp(firebaseConfig); // Initialize Firebase
 const firebaseAuth = getAuth(firebaseApp); // Initialize Firebase Authentication
 const googleProvider = new GoogleAuthProvider(); // Initialize Google Auth Provider
 
 export const FirebaseProvider = ({ children }) => {
 
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        onAuthStateChanged(firebaseAuth, (currentUser) => {
+            if (user) setUser(currentUser)
+            else setUser(null)
+        }, [])
+    });
     // Function to sign up a user with email and password
     const signupUserWithEmailAndPassword = (email, password) => createUserWithEmailAndPassword(firebaseAuth, email, password);
 
@@ -38,10 +46,12 @@ export const FirebaseProvider = ({ children }) => {
     // Function to sign in with Google
     const signInWithGoogle = () => signInWithPopup(firebaseAuth, googleProvider);
 
+    const isLoggedIn = user ? true : false;
+
 
 
     return (
-        <FirebaseContext.Provider value={{ app: firebaseApp, signupUserWithEmailAndPassword, signinUserWithEmailAndPassword, signInWithGoogle }}>
+        <FirebaseContext.Provider value={{ app: firebaseApp, signupUserWithEmailAndPassword, signinUserWithEmailAndPassword, signInWithGoogle, isLoggedIn }}>
             {children}
         </FirebaseContext.Provider>
     );
