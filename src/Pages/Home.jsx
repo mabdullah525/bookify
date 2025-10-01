f4eb49b2f3230bfcd911b1b88fe4ae007ebe3813
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-10-01 10:57:22 -0700

    Simplify book data fetching in Home component and update rendering structure

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,28 +2,19 @@ import React, { useEffect, useState } from 'react'
 import { useFirebase } from '../Context/firebase'
 
 const Home = () => {
+
     const [books, setBooks] = useState([]);
+
     const firebase = useFirebase();
-
     useEffect(() => {
-        firebase.listAllBooks().then((snapshot) => {
-            setBooks(snapshot.docs);
-        });
+        firebase.listAllBooks().then((books) => setBooks(books.docs));
     }, []);
-
     return (
-        <div className="p-6">
-            <h2 className="text-2xl font-bold mb-4 text-white">📚 Book Listings</h2>
-            <ul className="space-y-2">
-                {books.map((book) => (
-                    <li 
-                        key={book.id} 
-                        className="p-3 bg-gray-800 rounded-lg text-gray-100 shadow"
-                    >
-                        {book.data().name}
-                    </li>
-                ))}
-            </ul>
+        <div>
+            {
+                books.map(book => <li>{book.data().name}</li>)
+            }
+
         </div>
     )
 }
