dc567750d8b28e733262b4bab279cd55577574e3
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-10-05 13:42:51 -0700

    All done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
+import React from "react";
 
 const Navbar = () => {
     return (
         <div className="navbar">
             <div className="logo">📚 Bookify</div>
             <div className="nav-links">
-                <a href="/" className="nav-link active">Home</a>
-                <NavLink to="/list" className="nav-link">Book Listing</NavLink>
-                <NavLink to="/orders" className="nav-link">Orders</NavLink>
+                <NavLink
+                    to="/"
+                    end
+                    className={({ isActive }) =>
+                        isActive ? "nav-link active" : "nav-link"
+                    }
+                >
+                    Home
+                </NavLink>
+
+                <NavLink
+                    to="/list"
+                    className={({ isActive }) =>
+                        isActive ? "nav-link active" : "nav-link"
+                    }
+                >
+                    Book Listing
+                </NavLink>
+
+                <NavLink
+                    to="/orders"
+                    className={({ isActive }) =>
+                        isActive ? "nav-link active" : "nav-link"
+                    }
+                >
+                    Orders
+                </NavLink>
             </div>
         </div>
+    );
+};
 
-
-    )
-}
-
-export default Navbar
+export default Navbar;
